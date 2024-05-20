b5c22fc63cbc36b7b5a6c00de4517787e95a93ac
RamaRizki23 <rama23rizki04@gmail.com>
2024-05-20 13:30:20 +0700

    commit

---
+++ adminpanel/navbar.php
@@ -0,0 +1,36 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">NeoFarma</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="../adminpanel">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="kategori.php">Category</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="produk.php">Product</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Article
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="mental">Mental Health</a></li>
+            <li><a class="dropdown-item" href="healthy">Healthy Lifestyle</a></li>
+            <li><a class="dropdown-item" href="parent">Parenting</a></li>
+            <li><a class="dropdown-item" href="sex">Sex Education</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
