d84eebfbe8851dbd369a659a856dcb88ae115869
RamaRizki23 <rama23rizki04@gmail.com>
2024-05-26 10:32:42 +0700

    commit

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-    require "koneksi.php";
-    $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM produk LIMIT 5");
+require "koneksi.php";
+
+// Secure query using prepared statements
+$queryProduk = $con->prepare("SELECT id, nama, harga, foto, detail FROM produk LIMIT 5");
+$queryProduk->execute();
+$resultProduk = $queryProduk->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +20,7 @@
 <body>
     <?php require "navbar.php"; ?>
     
-    <!-- banner -->
+    <!-- Banner -->
     <div class="container-fluid banner d-flex align-items-center">
         <div class="container text-center text-white">
             <h1>Drugstore and Articles</h1>
@@ -24,7 +28,7 @@
             <div class="col-md-8 offset-md-2">
                 <form method="get" action="produk.php">
                     <div class="input-group input-group-lg my-4">
-                        <input type="text" class="form-control" placeholder="Product Name" aria-label="Recipient's username" aria-describedby="basic-addon2" name="keyword">
+                        <input type="text" class="form-control" placeholder="Product Name" aria-label="Product Name" name="keyword">
                         <button type="submit" class="btn warna4 text-white">Search</button>
                     </div>
                 </form>
@@ -32,11 +36,10 @@
         </div>
     </div>
 
-    <!-- highlighted kategori -->
+    <!-- Highlighted Categories -->
     <div class="container-fluid py-5">
         <div class="container text-center">
             <h3>Best seller</h3>
-
             <div class="row mt-5">
                 <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-anxiety d-flex justify-content-center align-items-center">
@@ -45,41 +48,40 @@
                 </div>
                 <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-filosofi d-flex justify-content-center align-items-center">
-                    <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=buku">Buku Filosofi Teras</a></h4>
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=buku">Buku Filosofi Teras</a></h4>
                     </div>
                 </div>
                 <div class="col-md-4 mb-3">
                     <div class="highlighted-kategori kategori-kes-mental d-flex justify-content-center align-items-center">
-                    <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=buku">Buku Kesehatan Mental</a></h4>
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=buku">Buku Kesehatan Mental</a></h4>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- tentang kami -->
+    <!-- About Us -->
     <div class="container-fluid warna5 py-5">
         <div class="container text-center">
             <h3>About Us</h3>
-            <p class="fs-5 mt-3">Welcome to the NeoFarma Website, this website is intended to sell medicines and also books and articles online, for those of you who want to know about mental health and so on, this website is very suitable for you, on this website there are also several medicines that are sold legally.</p>
+            <p class="fs-5 mt-3">Welcome to the NeoFarma Website. This website is intended to sell medicines and also books and articles online. For those of you who want to know about mental health and more, this website is very suitable for you. There are also several medicines sold legally here.</p>
         </div>
     </div>
 
-    <!-- produk -->
+    <!-- Products -->
     <div class="container-fluid py-5">
         <div class="container text-center">
-            <h3>Product</h3>
-
+            <h3>Products</h3>
             <div class="row mt-5">
-                <?php while($data = mysqli_fetch_array($queryProduk)){ ?>
+                <?php while($data = $resultProduk->fetch_assoc()){ ?>
                 <div class="col-sm-6 col-md-4 mb-3">
                     <div class="card">
-                        <img src="image/<?php echo $data['foto']; ?>" class="card-img-top" alt="<?php echo $data['nama']; ?>">
+                        <img src="image/<?php echo ($data['foto']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($data['nama']); ?>">
                         <div class="card-body">
-                            <h4 class="card-title"><?php echo $data['nama']; ?></h4>
-                            <p class="card-text text-truncate"><?php echo $data['detail']; ?></p>
+                            <h4 class="card-title"><?php echo ($data['nama']); ?></h4>
+                            <p class="card-text text-truncate"><?php echo ($data['detail']); ?></p>
                             <p class="card-text text-harga">Rp.<?php echo number_format($data['harga'], 0, ',', '.'); ?></p>
-                            <a href="detail_produk.php?id=<?php echo $data['id']; ?>" class="btn warna4">View Details</a>
+                            <a href="#" class="btn warna4 text-white">View Details</a>
                         </div>
                     </div>
                 </div>
