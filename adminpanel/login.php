4c45cbec42250816dd51ec74106e2b967601dfa3
RamaRizki23 <rama23rizki04@gmail.com>
2024-06-04 14:02:43 +0700

    commit

--- adminpanel/login.php
+++ adminpanel/login.php
@@ -8,7 +8,7 @@ require "../koneksi.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="../boostrap/css/boostrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -63,7 +63,7 @@ require "../koneksi.php";
                     else{
                         ?>
                     <div class="alert alert-danger" role="alert">
-  Password salah!
+                    Password wrong!
 </div>
                     <?php
                     }
@@ -71,7 +71,7 @@ require "../koneksi.php";
                 else{
                     ?>
                     <div class="alert alert-warning" role="alert">
-  Akun tidak tersedia!
+                    Account not available!
 </div>
                     <?php
                 }
